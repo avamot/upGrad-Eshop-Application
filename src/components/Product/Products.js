81c47e28cac44cfdeb2a1cb3a7ee4d021350897e
AVANTHI MOTURI <avanthi.moturi@gmail.com>
2024-05-11 10:29:54 +0530

    Added Filtering and Sorting Functionality for the products

--- src/components/Product/Products.js
+++ src/components/Product/Products.js
@@ -15,7 +15,6 @@ import CardActions from '@mui/material/CardActions';
 import { Grid } from '@material-ui/core';
 import { CardActionArea, CardMedia, Typography} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import Dropdown from '../DropDown/Dropdown';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -36,6 +35,9 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Products() {
   const [alignment, setAlignment] = useState('all');
+  const [value, setValue] = React.useState('');
+  let [newProducts, setNewProducts] = React.useState([]);
+  const [sortedProducts, setSortedProducts] = useState([]);
 
 
 
@@ -47,12 +49,15 @@ export default function Products() {
 
 
   React.useEffect(() => {
-    DisplayProducts();
+    GetProducts();
     console.log("products", products)
   }, []);
 
   const handleChange = (event, newAlignment) => {
-    setAlignment(newAlignment);
+    if (newAlignment != null){
+        setAlignment(newAlignment);
+    }
+    
     console.log(newAlignment);
 
   };
@@ -76,6 +81,8 @@ export default function Products() {
   
 
   const DisplayAllProducts = () => {
+    
+    if (value.toLowerCase().includes('')) {
     if (alignment.toLowerCase().includes('all')){
       return (
          <Grid container
@@ -120,8 +127,7 @@ export default function Products() {
        ))}
      </Grid>      
      )
-    }
-
+}
     else {
       return (<Grid container
         spacing={2}
@@ -167,10 +173,101 @@ export default function Products() {
      </Grid>      
      )
     }
-    
+} else {
+    if (alignment.toLowerCase().includes('all')){
+        return (
+           <Grid container
+          spacing={2}
+          direction="row"
+          justify="flex-start"
+          alignItems="stretch"
+          style={{ margin: "50px" }}>
+          {sortedProducts.map((product) => (
+           <Grid item md={4} key={product.id} style={{ display: 'flex' }}>
+             <Card sx={{ maxWidth: 300 }} className={classes.card} style={{ padding: "0", display: 'flex' }}>
+               <CardActionArea>
+                 <CardMedia
+                   component="img"
+                   alt="Contemplative Reptile"
+                   height="194"
+                   image={"" + product.imageUrl}
+                   title="Contemplative Reptile"
+     
+                 />
+                 <CardContent style={{ display: 'flex' }}>
+                   <Stack direction={"row"} justifyContent={"space-between"} margin={"5px"}>
+                     <Typography gutterBottom variant="h6" component="div">{product.name}</Typography>
+                     <Typography gutterBottom variant="h6" component="div">&#8377;{product.price}</Typography>
+                   </Stack>
+                   <Typography variant="body2" color="text.secondary" style={{ margin: "5px" }}>
+                     {product.description}
+                   </Typography>
+                 </CardContent>
+               </CardActionArea>
+               <CardActions style={{ marginTop: "auto" }}>
+                 <Button size="medium">Buy</Button>
+                 <Grid container justifyContent="flex-end">
+                   <Stack direction={"row"} spacing={2}>
+                     <EditIcon />
+                     <DeleteIcon />
+                   </Stack>
+                 </Grid>
+               </CardActions>
+             </Card>
+           </Grid>
+         ))}
+       </Grid>      
+       )
+  }
+      else {
+        return (<Grid container
+          spacing={2}
+          direction="row"
+          justify="flex-start"
+          alignItems="stretch"
+          style={{ margin: "50px" }}>
+          {(sortedProducts.filter(filteredProduct => 
+          filteredProduct.category.toLowerCase().includes(alignment.toLowerCase()))).map((product) => (
+           <Grid item md={4} key={product.id} style={{ display: 'flex' }}>
+             <Card sx={{ maxWidth: 300 }} className={classes.card} style={{ padding: "0", display: 'flex' }}>
+               <CardActionArea>
+                 <CardMedia
+                   component="img"
+                   alt="Contemplative Reptile"
+                   height="194"
+                   image={"" + product.imageUrl}
+                   title="Contemplative Reptile"
+     
+                 />
+                 <CardContent style={{ display: 'flex' }}>
+                   <Stack direction={"row"} justifyContent={"space-between"} margin={"5px"}>
+                     <Typography gutterBottom variant="h6" component="div">{product.name}</Typography>
+                     <Typography gutterBottom variant="h6" component="div">&#8377;{product.price}</Typography>
+                   </Stack>
+                   <Typography variant="body2" color="text.secondary" style={{ margin: "5px" }}>
+                     {product.description}
+                   </Typography>
+                 </CardContent>
+               </CardActionArea>
+               <CardActions style={{ marginTop: "auto" }}>
+                 <Button size="medium">Buy</Button>
+                 <Grid container justifyContent="flex-end">
+                   <Stack direction={"row"} spacing={2}>
+                     <EditIcon />
+                     <DeleteIcon />
+                   </Stack>
+                 </Grid>
+               </CardActions>
+             </Card>
+           </Grid>
+         ))}
+       </Grid>      
+       )
+      }
+}
   }
 
-  const DisplayProducts = async () => {
+  const GetProducts = async () => {
 
     try {
       console.log("inside");
@@ -178,6 +275,10 @@ export default function Products() {
       const prods = response.data;
       products = [...prods];
       setProducts(products);
+
+      newProducts = [...products];
+        setNewProducts(newProducts);
+
 
       console.log("products", products);
 
@@ -198,6 +299,74 @@ export default function Products() {
     console.log("response", res);
 
   }
+
+  const Dropdown = () => {
+
+   
+
+    const handleChange = (event) => {
+
+        setValue(event.target.value);
+        
+
+        if(event.target.value === '') {
+            const sortedProducts = products;
+            setSortedProducts([...sortedProducts]);
+            console.log("showProductList",[sortedProducts]);
+    
+        }
+        if(event.target.value === 'default') {
+            GetProducts();
+            const sortedProducts = products;
+            setSortedProducts([...products]);
+             console.log("showProductList",[sortedProducts]);
+    
+        }
+        
+        if(event.target.value === 'HighToLow') {
+            
+            const sortedProducts = products.sort((a, b) => b.price - a.price);
+            setSortedProducts([...sortedProducts]);
+        }
+    
+        if(event.target.value === 'LowToHigh') {
+            
+            const sortedProducts = products.sort((a, b) => a.price - b.price);
+            setSortedProducts([...sortedProducts]);
+        }
+
+        DisplayAllProducts();
+
+    };
+
+    return (
+
+        <div style={{ marginLeft: "100px" }}>
+
+            <label style={{ fontSize: '12px' }}>
+
+                Sort By:
+
+                <br /><select defaultValue="" value={value} style={{ width: "200px", height:"25px" }} onChange={handleChange}>
+                    <option disabled={true} value="">Select...</option>
+
+                    <option value="default">Default</option>
+
+                    <option value="HighToLow">Price: High to Low</option>
+
+                    <option value="LowToHigh">Price: Low to High</option>
+
+                    <option value="option3">Newest</option>
+
+                </select>
+
+            </label>
+
+        </div>
+
+    );
+
+};
  
   return (
     <>
